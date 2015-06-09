554132d90b30371f4238464f26d026ba621ff1bc
David Crosby <crosby@atomicobject.com>
2015-06-08 23:03:50 -0400

    RSchemaHamster.schema

--- lib/rschema_hamster.rb
+++ lib/rschema_hamster.rb
@@ -3,3 +3,8 @@ require 'hamster'
 require 'rschema_hamster/hamster_ext'
 require 'rschema_hamster/dsl'
 
+module RSchemaHamster
+  def self.schema(dsl=RSchemaHamster::DSL, &block)
+    RSchema.schema(dsl, &block)
+  end
+end
